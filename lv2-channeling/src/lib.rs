7d4a8ef501db745af501a24e10f0259c2f7bef00
Ondřej Hošek <ondra.hosek@gmail.com>
2023-03-18 09:07:21 +0100

    cutoff control values at halfway point

--- lv2-channeling/src/lib.rs
+++ lv2-channeling/src/lib.rs
@@ -27,22 +27,28 @@ impl Plugin for Channeling {
             .zip(ports.right.iter())
             .zip(ports.output.iter_mut());
         for ((left_frame, right_frame), out_frame) in zippy {
-            if mode_value < 1.0 {
+            // cutoff at halfway point, round ties up
+            // e.g. canonical value 1.0 => >= 0.5, < 1.5
+
+            if mode_value < -0.5 {
+                // misconfiguration
+                *out_frame = 0.0;
+            } else if mode_value < 0.5 {
                 // Average
                 *out_frame = (*left_frame + *right_frame) / 2.0;
-            } else if mode_value < 2.0 {
+            } else if mode_value < 1.5 {
                 // Left
                 *out_frame = *left_frame;
-            } else if mode_value < 3.0 {
+            } else if mode_value < 2.5 {
                 // Right
                 *out_frame = *right_frame;
-            } else if mode_value < 4.0 {
+            } else if mode_value < 3.5 {
                 // Out-of-Phase Stereo
                 *out_frame = *left_frame - *right_frame;
-            } else if mode_value < 5.0 {
+            } else if mode_value < 4.5 {
                 // Min
                 *out_frame = left_frame.min(*right_frame);
-            } else if mode_value < 6.0 {
+            } else if mode_value < 5.5 {
                 // Max
                 *out_frame = left_frame.max(*right_frame);
             } else {
